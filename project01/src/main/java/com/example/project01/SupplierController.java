d2fcbc6f4f3bbddedda73448a8df8827c5bb9859
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-07-16 12:48:15 +0530

    create supplier details dashboard

---
+++ project01/src/main/java/com/example/project01/SupplierController.java
@@ -0,0 +1,27 @@
+package com.example.project01;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class SupplierController {
+
+    public TextField txtTel;
+    public TextField txtAddress;
+    public TextField txtName;
+    public TextField txtId;
+    public TableView tableView;
+
+    public void addBtn(ActionEvent actionEvent) {
+    }
+
+    public void updateBtn(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtn(ActionEvent actionEvent) {
+    }
+
+    public void searchBtn(ActionEvent actionEvent) {
+    }
+}
