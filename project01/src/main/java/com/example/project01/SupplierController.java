6c931e341efa2e6581009e3c85beb02080ee97b1
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-07-16 19:52:44 +0530

    supplier dashboard contd.

--- project01/src/main/java/com/example/project01/SupplierController.java
+++ project01/src/main/java/com/example/project01/SupplierController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SupplierController {
 
     public TextField txtTel;
@@ -14,6 +17,20 @@ public class SupplierController {
     public TableView tableView;
 
     public void addBtn(ActionEvent actionEvent) {
+        //01. create a SQL
+        String SQL= "INSERT INTO Supplier VALUES('01','Amal','Colombo',0704056789)";
+
+        //02. Run the driver software
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //03. create the connection to the DB
+        Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/possystem_geethma","root","1234");
+
+        //04. create a statement
+
+        //05. Execute the sql
+
+
     }
 
     public void updateBtn(ActionEvent actionEvent) {
--- project01/src/main/java/module-info.java
+++ project01/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.project01 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql; // Required for JDBC
+    //requires mysql.connector.java; // MySQL Connector (JAR must be on module path)
 
 
     opens com.example.project01 to javafx.fxml;
