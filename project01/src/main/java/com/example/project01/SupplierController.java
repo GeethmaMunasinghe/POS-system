e3cf404fe253b3e933595708051a2b00a735cf3a
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-07-17 11:06:53 +0530

    searchBtn contd.

--- project01/src/main/java/com/example/project01/SupplierController.java
+++ project01/src/main/java/com/example/project01/SupplierController.java
@@ -144,13 +144,9 @@ public class SupplierController {
             //05. Execute the sql
             ResultSet result =preparedStatement.executeQuery();
             if (result.next()){
-                String name=result.getString("sname");
-                String address=result.getString("address");
-                int tel=result.getInt("tel");
-
-                txtName.setText(name);
-                txtAddress.setText(address);
-                txtTel.setText(Integer.toString(tel));
+                txtName.setText(result.getString("sname"));
+                txtAddress.setText(result.getString("address"));
+                txtTel.setText(String.valueOf(result.getInt("tel")));
             }
 
         }catch (Exception e){
