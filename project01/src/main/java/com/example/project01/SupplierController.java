32306d7c6b01c6d49f718133164791266164c870
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-07-17 11:03:15 +0530

    completed searchBtn

--- project01/src/main/java/com/example/project01/SupplierController.java
+++ project01/src/main/java/com/example/project01/SupplierController.java
@@ -5,10 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SupplierController {
 
@@ -128,5 +125,37 @@ public class SupplierController {
     }
 
     public void searchBtn(ActionEvent actionEvent) {
+        String id = txtId.getText();
+
+        try {
+            //01. create a SQL
+            String SQL= "SELECT * from Supplier WHERE sid=?";
+
+            //02. Run the driver software
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //03. create the connection to the DB
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/possystem_geethma","root","1234");
+
+            //04. create a statement
+            PreparedStatement preparedStatement=connection.prepareStatement(SQL);
+            preparedStatement.setString(1,id);
+
+            //05. Execute the sql
+            ResultSet result =preparedStatement.executeQuery();
+            if (result.next()){
+                String name=result.getString("sname");
+                String address=result.getString("address");
+                int tel=result.getInt("tel");
+
+                txtName.setText(name);
+                txtAddress.setText(address);
+                txtTel.setText(Integer.toString(tel));
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
